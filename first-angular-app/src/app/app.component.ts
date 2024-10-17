9cb64da1a3d97534fb89e79bc2a85e7bf949c208
Adarsh suryavanshi <adarshsuryavanshi3@gmail.com>
2024-10-18 00:24:47 +0530

    understand if and for loop  interface

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.Component';
 import { UserComponent } from './user/user.Component';
-import {DUMMY_USERS} from './dummy-user'
+import { DUMMY_USERS } from './dummy-user'
 
-
-
+import { CommonModule } from '@angular/common';
+import { TasksComponent } from './tasks/task.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   users = DUMMY_USERS
+  selectedUserId = 'u1'
+  userSelected?: any;
+
+  get selectedUser() {
+    console.log(this.users.find((user) => user.id === this.selectedUserId))
+    return this.users.find((user) => user.id === this.selectedUserId)!
+  }
+
+  onSelectUser(id: string) {
+    console.log(id)
+    this.selectedUserId = id
+    console.log(id)
+  }
 
 }
--- first-angular-app/src/app/user/user.Component.ts
+++ first-angular-app/src/app/user/user.Component.ts
@@ -1,4 +1,17 @@
-import { Component, computed, Input, signal } from "@angular/core";
+import { Component, EventEmitter, Input, Output, } from "@angular/core";
+
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string
+// }
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string
+}
 
 @Component({
   selector: 'app-user',
@@ -10,15 +23,20 @@ import { Component, computed, Input, signal } from "@angular/core";
 })
 
 export class UserComponent {
+  @Input({ required: true }) user!: User
+  @Output() select = new EventEmitter()
 
-  @Input() avatar!: string
-  @Input() name!: string
-
+  // avatar = input.required<string>()
+  // name =  input.required<string>()
+  // imagePath = computed(()=>{ī
+  //   return 'assets/users/' + this.avatar()
+  // })
   get imagePath() {
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
   onSelectUser() {
-
+    console.log(this.user.id)
+    this.select.emit(this.user.id)
   }
 
 }
